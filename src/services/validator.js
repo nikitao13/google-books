1a7cc6b86a08d42e971757c652730c9fd23826e9
Nikita O'Keeffe <okeeffe.nikita@gmail.com>
2025-01-12 10:44:04 +1100

    removed unecessary files

--- src/services/validator.js
+++ src/services/validator.js
@@ -12,7 +12,6 @@ const validateForm = (formData) => {
     error = "search must have at least 3 characters."
   }
   
-  // Check for too long of a query
   if(formData.length > 20) {
     isValid = false;
     error = "search must be less than 20 characters."
